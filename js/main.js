19c34c350ff5298c96a3391532f854b5e4e10970
Bonnie <73203899+Bonnie39@users.noreply.github.com>
2023-12-09 12:59:09 -0600

    update

--- js/main.js
+++ js/main.js
@@ -1,75 +1,69 @@
-$(document).ready(function() {
+const tiltEffectSettings = {
+    max: 25, // max tilt rotation (degrees (deg))
+    perspective: 1000, // transform perspective, the lower the more extreme the tilt gets (pixels (px))
+    scale: 1.1, // transform scale - 2 = 200%, 1.5 = 150%, etc..
+    speed: 500, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
+    easing: "cubic-bezier(.03,.98,.52,.99)" // easing (transition-timing-function) of the enter/exit transition
+  };
+  
+  const cards = document.querySelectorAll(".card");
+  
+  cards.forEach(card => {
+    card.addEventListener("mouseenter", cardMouseEnter);
+    card.addEventListener("mousemove", cardMouseMove);
+    card.addEventListener("mouseleave", cardMouseLeave);
+  });
+  
+  function cardMouseEnter(event) {
+    setTransition(event);
+  }
+  
+  function cardMouseMove(event) {
+    const card = event.currentTarget;
+    const cardWidth = card.offsetWidth;
+    const cardHeight = card.offsetHeight;
+    const centerX = card.offsetLeft + cardWidth/2;
+    const centerY = card.offsetTop + cardHeight/2;
+    const mouseX = event.clientX - centerX;
+    const mouseY = event.clientY - centerY;
+    const rotateXUncapped = (+1)*tiltEffectSettings.max*mouseY/(cardHeight/2);
+    const rotateYUncapped = (-1)*tiltEffectSettings.max*mouseX/(cardWidth/2);
+    const rotateX = rotateXUncapped < -tiltEffectSettings.max ? -tiltEffectSettings.max : 
+                    (rotateXUncapped > tiltEffectSettings.max ? tiltEffectSettings.max : rotateXUncapped);
+    const rotateY = rotateYUncapped < -tiltEffectSettings.max ? -tiltEffectSettings.max : 
+                    (rotateYUncapped > tiltEffectSettings.max ? tiltEffectSettings.max : rotateYUncapped);
+  
+    card.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) 
+                            scale3d(${tiltEffectSettings.scale}, ${tiltEffectSettings.scale}, ${tiltEffectSettings.scale})`;
+  }
+  
+  function cardMouseLeave(event) {
+    event.currentTarget.style.transform = `perspective(${tiltEffectSettings.perspective}px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`;
+    setTransition(event);
+  }
+  
+  function setTransition(event) {
+    const card = event.currentTarget;
+    clearTimeout(card.transitionTimeoutId);
+    card.style.transition = `transform ${tiltEffectSettings.speed}ms ${tiltEffectSettings.easing}`;
+    card.transitionTimeoutId = setTimeout(() => {
+      card.style.transition = "";
+    }, tiltEffectSettings.speed);
+  }
 
-    if($.cookie("ody-theme") == 1){
-        $(".hero-jumbo").css({background:'linear-gradient(242deg, #ff8a4b, #cf86fa) 0% 0% / 200% '});
-        $(".hero-jumbo").css({animation:'9s ease 0s infinite normal none running AnimationName'});
-    }
-    
-    if($.cookie("ody-theme") == 2){
-    
-    
-        $(".hero-jumbo").css({background:'linear-gradient(242deg, #8454a8, #98587d,#af5c4c) 0% 0% / 200% '});
-        $(".hero-jumbo").css({animation:'9s ease 0s infinite normal none running AnimationName'});
-    }
-    
-    if($.cookie("ody-theme") == 3){
-        
-    
-        $(".hero-jumbo").css({background:'linear-gradient(242deg, #534548, #da0628) 0% 0% / 200% '});
-        $(".hero-jumbo").css({animation:'9s ease 0s infinite normal none running AnimationName'});
-    }
-    
-    if(!$.cookie("ody-theme")){
-        $(".hero-jumbo").css({background:'linear-gradient(242deg, #ff8a4b, #cf86fa) 0% 0% / 200% '});
-        $(".hero-jumbo").css({animation:'9s ease 0s infinite normal none running AnimationName'});
-    }
-    
-    
-    
-    $("#classic").on('click',function(){
-    
-        $.cookie("ody-theme", 1);
-        $(".hero-jumbo").css({background:'linear-gradient(242deg, #ff8a4b, #cf86fa) 0% 0% / 200% '});
-        $(".hero-jumbo").css({animation:'9s ease 0s infinite normal none running AnimationName'});
-    
-    
-     });
-    
-     $("#dark").on('click',function(){
-    
-        $.cookie("ody-theme", 2);
-    
-        $(".hero-jumbo").css({background:'linear-gradient(242deg, #8454a8, #98587d,#af5c4c) 0% 0% / 200% '});
-        $(".hero-jumbo").css({animation:'9s ease 0s infinite normal none running AnimationName'});
-     });
-    
-     $("#Meridian").on('click',function(){
-    
-        $.cookie("ody-theme", 3);
-    
-        $(".hero-jumbo").css({background:'linear-gradient(242deg, #534548, #da0628) 0% 0% / 200% '});
-        $(".hero-jumbo").css({animation:'9s ease 0s infinite normal none running AnimationName'});
-     });
-     
-    
-        if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-            //i.e. apply safari class via jquery
-            $(".navbar").addClass("fancy");
-        }
-    
-        if ($("#hero-container").length > 0) {
-            window.addEventListener("scroll", function() {
-                if ($(window).scrollTop() > $("#hero-container").height()) {
-                    $("body").addClass("page-scrolled");
-                    $(".navbar").removeClass("clearbg");
-                } else {
-                    $("body").removeClass("page-scrolled");
-                    $(".navbar").addClass("clearbg");
-                }
-            }, { passive: true });
-        }
-        console.log(window.location.pathname);
+  document.querySelectorAll(".UNUSED").forEach((b) => {
+    console.log(b);
+    b.onmouseleave = (e) => {
+      e.target.style.background = "black";
+      e.target.style.borderImage = null;
+    };
+
+  
+    b.addEventListener("mousemove", (e) => {
+      const rect = e.target.getBoundingClientRect();
+      const x = e.clientX - rect.left; //x position within the element.
+      const y = e.clientY - rect.top; //y position within the element.
+      e.target.style.background = `radial-gradient(circle at ${x}px ${y}px , rgba(255,255,255,0.2),rgba(255,255,255,0) )`;
+      e.target.style.borderImage = `radial-gradient(20% 75% at ${x}px ${y}px ,rgba(255,255,255,0.7),rgba(255,255,255,0.1) ) 1 / 1px / 0px stretch `;
     });
-    
-    
-    
+  });
